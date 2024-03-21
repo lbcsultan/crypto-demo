42e540de1e1cb5b8eff1ba523c9d6721bd82cc28
Byoungcheon Lee <lbcsultan@gmail.com>
2024-03-21 10:32:55 +0900

    2nd

--- src/lib/computeHmac.ts
+++ src/lib/computeHmac.ts
@@ -2,7 +2,8 @@ import forge from 'node-forge'
 
 const computeHmac = (algorithm: string, inputText: string, secret: string) => {
   let hmac = forge.hmac.create()
-  hmac.start(algorithm, secret)
+  let algo = algorithm as forge.md.Algorithm
+  hmac.start(algo, secret)
   hmac.update(inputText)
   return hmac.digest().toHex()
 }
